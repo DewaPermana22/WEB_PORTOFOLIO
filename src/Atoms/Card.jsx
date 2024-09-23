4c1de5aed7007b02871c205f4be85fc9e950517e
DewaPermana22 <dewarisa77@gmail.com>
2024-09-23 12:29:39 +0700

    solved image

--- src/Atoms/Card.jsx
+++ src/Atoms/Card.jsx
@@ -1,8 +1,9 @@
 import { motion } from "framer-motion"
-const Card = ({logo, ukuran, warna, NamaBahasa}) => {
+const Card = ({logo, ukuran, warna, NamaBahasa,logo1}) => {
     return(
         <motion.div whileHover={{scale: 1.1,backgroundColor: '#6B21A8', color: '#ffffff'}} className="flex flex-col items-center gap-2 border-2 border-slate-50 p-5 w-[150px] h-auto rounded-lg shadow-lg bg-slate-50">
             <h1 className={`${ukuran} ${warna}`}>{logo}</h1>
+            <img src={logo1} className="w-[60px] h-[60px]" alt="" />
             <h1 className="text-lg font-bold font-Signika">{NamaBahasa}</h1>            
         </motion.div>
     )
